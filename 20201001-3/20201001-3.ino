f78f41838d030a084847d20ed96f379c645feaa5
sutesam <115021553+sutesam@users.noreply.github.com>
2025-10-01 15:32:37 +0800

    20251001-serial

--- 20201001-3/20201001-3.ino
+++ 20201001-3/20201001-3.ino
@@ -5,6 +5,8 @@ const int BledPin = 5;
 
 int buttonState = 0;
 int ledcolor = 0;
+bool ButtonPressed = false;
+String currentcolor="led";
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,53 +14,67 @@ void setup() {
   pinMode(GledPin, OUTPUT);
   pinMode(BledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   buttonState = digitalRead(buttonPin);
-
-  if(buttonState == HIGH) {
+  Serial.print("Current Color: ");
+  Serial.println(currentcolor);
+  if(buttonState == HIGH && !ButtonPressed) {
     ledcolor = ledcolor + 1;
-    delay(100);
+    ButtonPressed = true;
+    //delay(100);
+  }
+  if(buttonState == LOW && ButtonPressed) {
+    ButtonPressed = false;
   }
 
   if(ledcolor == 0){
+    currentcolor="LED off";
     digitalWrite(RledPin, HIGH);
     digitalWrite(GledPin, HIGH);
     digitalWrite(BledPin, HIGH);
   }
   else if(ledcolor == 1){
+    currentcolor="Red";
     digitalWrite(RledPin, LOW);
     digitalWrite(GledPin, HIGH);
     digitalWrite(BledPin, HIGH);
   }
   else if(ledcolor == 2){
+    currentcolor="Green";
     digitalWrite(RledPin, HIGH);
     digitalWrite(GledPin, LOW);
     digitalWrite(BledPin, HIGH);
   }
   else if(ledcolor == 3){
+    currentcolor="Blue";
     digitalWrite(RledPin, HIGH);
     digitalWrite(GledPin, HIGH);
     digitalWrite(BledPin, LOW);
   }
     else if(ledcolor == 4){
+    currentcolor="Yellow";
     digitalWrite(RledPin, LOW);
     digitalWrite(GledPin, LOW);
     digitalWrite(BledPin, HIGH);
   }
     else if(ledcolor == 5){
+      currentcolor="Purple";
     digitalWrite(RledPin, LOW);
     digitalWrite(GledPin, HIGH);
     digitalWrite(BledPin, LOW);
   }
     else if(ledcolor == 6){
+      currentcolor="Cyan";
     digitalWrite(RledPin, HIGH);
     digitalWrite(GledPin, LOW);
     digitalWrite(BledPin, LOW);
   }
     else if(ledcolor == 7){
+      currentcolor="White";
     digitalWrite(RledPin, LOW);
     digitalWrite(GledPin, LOW);
     digitalWrite(BledPin, LOW);
